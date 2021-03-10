5de13efdfb4cfedbc084fbd32b507dd2aef027c3
brillet.k@gmail.com <>
2021-03-10 15:35:24 +0100

    Create Evaluation.cpp

---
+++ Evaluation.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <iostream>
+#include <vector>
+#include "course.h"
+#include "course.cpp"
+
+using namespace std ;
+
+int main(){
+    std::cout << "_____________________________________________________________" << endl ;
+    Joueur JoueurUn = Joueur ("Michelle", 3);
+};
